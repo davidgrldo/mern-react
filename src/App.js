a4140c2d3326f21a2291a0f08d683740b028c994
David Geraldo <50208233+davidgrldo@users.noreply.github.com>
2021-02-02 06:30:40 +0700

    Create Header Component and Put to arrange LandingPage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react'
-import 'assets/scss/style.scss'
+import React from "react";
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import "assets/scss/style.scss";
+import LandingPage from "pages/LandingPage";
 
 function App() {
   return (
-    <div className="App">
-      Hello, world!
-    </div>
-  );
+  <div className="App">
+    <Router>
+      <Route path="/" component={LandingPage}></Route>
+    </Router>
+  </div>
+  )
 }
 
 export default App;
